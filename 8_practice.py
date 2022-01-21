939f6c4ec354e8b963acc1e8ec6c1484bea372e3
DonghaeSuh <gypsi12@naver.com>
2022-01-21 22:51:49 +0900

    20220121

--- 8_practice.py
+++ 8_practice.py
@@ -115,3 +115,30 @@
 # score_file.close()
 
 #pickle-----------------------------------------------------------
+# import pickle # pickle 모듈 가져다 쓰기
+
+# profile_file = open("profile.pickle", "wb") # 바이너리(binary) 형태로 저장
+# profile = {"이름":"박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) # profile 데이터를 file 에 저장
+# profile_file.close()
+
+
+# profile_file = open("profile.pickle", "rb") # 읽을 때에도 바이너리(binary) 명시
+# profile = pickle.load(profile_file) # file 에 있는 정보를 불러와서 profile 에 저장
+
+# print(profile)
+# profile_file.close()
+
+# wiht -------------------------------------------------------------
+# import pickle
+
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+
+# with open("study.txt", "r", encoding="utf-8") as study_file:
+#     print(study_file.read())
+
